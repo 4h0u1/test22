f69341122fb364546260083508252ea5fc58f52a
4h0u1 <116796774+4h0u1@users.noreply.github.com>
2023-10-31 15:10:54 +0300

    Add files via upload

---
+++ треугольники калькулятор/треугольники/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace треугольники
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(13, 63);
+            textBox1.Margin = new Padding(2, 1, 2, 1);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(13, 104);
+            textBox2.Margin = new Padding(2, 1, 2, 1);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(13, 148);
+            textBox3.Margin = new Padding(2, 1, 2, 1);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Firebrick;
+            button1.FlatAppearance.BorderColor = Color.Black;
+            button1.FlatAppearance.MouseDownBackColor = Color.Maroon;
+            button1.FlatAppearance.MouseOverBackColor = Color.Maroon;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Verdana", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(11, 187);
+            button1.Margin = new Padding(2, 1, 2, 1);
+            button1.Name = "button1";
+            button1.Size = new Size(488, 47);
+            button1.TabIndex = 3;
+            button1.Text = "Рассчитать треугольник";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.DarkRed;
+            label1.Font = new Font("Verdana", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(13, 12);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(495, 31);
+            label1.TabIndex = 4;
+            label1.Text = "Введите длины сторон треугольника";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Verdana", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(174, 65);
+            label2.Margin = new Padding(2, 0, 2, 0);
+            label2.Name = "label2";
+            label2.RightToLeft = RightToLeft.Yes;
+            label2.Size = new Size(0, 25);
+            label2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Firebrick;
+            button2.FlatAppearance.BorderColor = Color.Black;
+            button2.FlatAppearance.MouseDownBackColor = Color.Maroon;
+            button2.FlatAppearance.MouseOverBackColor = Color.Maroon;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Verdana", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(11, 236);
+            button2.Margin = new Padding(2, 1, 2, 1);
+            button2.Name = "button2";
+            button2.Size = new Size(488, 47);
+            button2.TabIndex = 6;
+            button2.Text = "Завершение программы";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Maroon;
+            ClientSize = new Size(510, 288);
+            ControlBox = false;
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Margin = new Padding(2, 1, 2, 1);
+            Name = "Form1";
+            Text = "Кальулятор для треугольника";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Button button2;
+    }
+}
